773cc0c6ca79ae3439112eeaa71b89d6a94f621f
Justin Stanley <jtst@iastate.edu>
2021-10-28 22:53:44 -0500

    nuke version command

--- threebot/commands/version.py
+++
@@ -1,8 +0,0 @@
-# Version command
-
-usage = 'Gets the current bot version.'
-
-import pkg_resources
-
-def execute(data, argv):
-    data.reply('Bot version: {}'.format(pkg_resources.get_distribution('threebot').version))
