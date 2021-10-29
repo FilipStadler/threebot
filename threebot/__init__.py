179e61786d226ffaddd283973e0914170d265955
Justin Stanley <jtst@iastate.edu>
2021-10-28 22:06:52 -0500

    add gplay command, move method to util

--- threebot/__init__.py
+++ threebot/__init__.py
@@ -28,31 +28,7 @@ parser.add_argument('--pw', default=PASS, help='Connection password')
 
 args = parser.parse_args()
 
-def play_sound_or_alias(name):
-    """Tries to play a sound or an alias to a sound."""
-    c = db.conn.cursor()
 
-    # check if sound is valid code
-    c.execute('SELECT * FROM sounds WHERE soundname=?', [name])
-
-    if len(c.fetchall()) > 0:
-        audio.play(name)
-    else:
-        # try and resolve as an alias
-        c.execute('SELECT * FROM aliases WHERE commandname=?', [name])
-
-        r2 = c.fetchall()
-
-        if len(r2) == 0:
-            raise Exception('"{0}" is not a recognized sound or alias'.format(name))
-        else:
-            # check that the alias plays a sound
-            action = r2[0][1].split(' ')
-
-            if action[0] != '!s':
-                raise Exception('"{0}" aliases to "{1}" which does not play a sound'.format(name, r2[0][1]))
-            else:
-                audio.play(action[1])
 
 def run():
     # Connect to server.
@@ -133,7 +109,7 @@ def run():
 
         if len(res) > 0:
             try:
-                play_sound_or_alias(res[1])
+                util.play_sound_or_alias(res[1])
             except Exception as e:
                 data.send_text_message('Error in greeting: {0}'.format(str(e)))
 
--- threebot/util.py
+++ threebot/util.py
@@ -1,5 +1,8 @@
 # Generic utilities for use by all commands.
 
+from . import audio
+from . import db
+
 def into_pages(headers, rows, rows_per_page=32):
     pages = []
 
@@ -23,3 +26,29 @@ def into_pages(headers, rows, rows_per_page=32):
         rows = rows[rows_per_page:]
 
     return pages
+
+def play_sound_or_alias(name):
+    """Tries to play a sound or an alias to a sound."""
+    c = db.conn.cursor()
+
+    # check if sound is valid code
+    c.execute('SELECT * FROM sounds WHERE soundname=?', [name])
+
+    if len(c.fetchall()) > 0:
+        audio.play(name)
+    else:
+        # try and resolve as an alias
+        c.execute('SELECT * FROM aliases WHERE commandname=?', [name])
+
+        r2 = c.fetchall()
+
+        if len(r2) == 0:
+            raise Exception('"{0}" is not a recognized sound or alias'.format(name))
+        else:
+            # check that the alias plays a sound
+            action = r2[0][1].split(' ')
+
+            if action[0] != '!s':
+                raise Exception('"{0}" aliases to "{1}" which does not play a sound'.format(name, r2[0][1]))
+            else:
+                audio.play(action[1])
