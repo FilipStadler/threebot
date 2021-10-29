ea8e524e06663b8b37aabb3a553ffef5f3bafccc
Justin Stanley <jtst@iastate.edu>
2021-10-28 21:44:16 -0500

    update alias exceptions

--- threebot/commands/alias.py
+++ threebot/commands/alias.py
@@ -5,14 +5,14 @@ usage = 'alias <aliasname> <command>'
 
 def execute(data, argv):
     if len(argv) < 2:
-        raise Exception('alias: expected 2 arguments, found {0}'.format(len(argv) - 1))
+        raise Exception('expected 2 arguments, found {0}'.format(len(argv) - 1))
 
     commandname = argv[0]
     action = ' '.join(argv[1:])
 
     # check if alias already exists
     if data.db.resolve_alias(commandname) is not None:
-        raise Exception('alias: destination alias "{0}" already exists!'.format(commandname))
+        raise Exception('destination alias "{0}" already exists!'.format(commandname))
 
     # create new alias
     c = data.db.conn.cursor()
