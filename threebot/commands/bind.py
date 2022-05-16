9af75746e91430cd854889d6b292533a380cb2df
quigley-c <carson.ll.quigley@gmail.com>
2022-05-15 20:15:21 -0400

    merge 'bind' and 'rebind' commands

--- threebot/commands/bind.py
+++ threebot/commands/bind.py
@@ -1,7 +1,7 @@
 # Custom bind command.
 
-desc = 'Plays your bind sound.'
-usage = 'bind'
+desc = 'Plays your bind sound. Rebinds on input.'
+usage = 'bind [CODE|ALIAS]'
 
 def execute(data, argv):
     # search for user bind
@@ -10,8 +10,16 @@ def execute(data, argv):
 
     results = c.fetchone()
 
+    # check if binding or rebinding
+    c.execute('SELECT * FROM binds WHERE username=?', [data.author])
     if results is None:
-        raise Exception('Your bind is not set! Assign it with \'!bind\'.')
+        c.execute('INSERT INTO binds VALUES (?, ?)', [data.author, argv[0]])
+    else:
+        c.execute('UPDATE binds SET bind=? WHERE username=?', [argv[0], data.author])
+
+    data.db.conn.commit()
+    data.reply('Set bind to {0}.'.format(argv[0]))
 
     data.util.play_sound_or_alias(results[0])
     data.reply('Playing bind: {}'.format(results[0]))
+
