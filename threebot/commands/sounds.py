fc3fccf551cd9fc7aaa7f38f1fc39809a37f5c51
codeandkey <jtst@iastate.edu>
2022-10-24 11:39:37 -0500

    sounds: formatting

--- threebot/commands/sounds.py
+++ threebot/commands/sounds.py
@@ -8,8 +8,9 @@ def execute(data, argv):
     c = data.db.conn.cursor()
     c.execute('SELECT * FROM sounds ORDER BY timestamp DESC')
     rows = c.fetchall()
+    headers = ['Sound', 'Author', 'Created', 'Source', 'Start', 'Length']
 
-    pages = data.util.into_pages(['Sound', 'Author', 'Created', 'Source', 'Start', 'Length'], rows)
+    pages = data.util.into_pages(headers, rows)
     selected = int(argv[0]) - 1 if len(argv) > 0 else 0
 
     if selected < 0 or selected >= len(pages):
