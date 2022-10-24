f5a9cbd6eb4ebf2fdc078cd7ab23c147e3a6b4c4
codeandkey <jtst@iastate.edu>
2022-10-24 11:36:04 -0500

    groups: formatting

--- threebot/commands/groups.py
+++ threebot/commands/groups.py
@@ -7,12 +7,13 @@ def execute(data, argv):
     c = data.db.conn.cursor()
     c.execute('SELECT * FROM groups ORDER BY groupname')
     rows = c.fetchall()
+    headers = ['Group', 'Content', 'Author', 'Created']
 
-    pages = data.util.into_pages(['Group', 'Content', 'Author', 'Created'], rows)
+    pages = data.util.into_pages(headers, rows)
     selected = int(argv[0]) - 1 if len(argv) > 0 else 0
 
     if selected < 0 or selected >= len(pages):
         raise Exception('invalid page number')
 
-    data.reply('Showing page {} of {}'.format(selected + 1, len(pages)))
+    data.reply(f'Showing page {selected + 1} of {len(pages)}')
     data.reply(pages[selected])
