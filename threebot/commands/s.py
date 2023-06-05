9192ffc721835ecf9d28145532c17599744efaea
Justin Stanley <jtst@iastate.edu>
2023-06-05 13:48:19 -0500

    s usage formatting

--- threebot/commands/s.py
+++ threebot/commands/s.py
@@ -1,7 +1,7 @@
 # Sound play command.
 
 desc = "Plays a sound from the local collection."
-usage = "s [CODE [{normal|fast|slow|muffle}]]"
+usage = "s [CODE] [normal|fast|slow|muffle]*"
 
 def execute(data, argv):
     target = data.db.random_sound() if len(argv) < 1 else argv[0]
